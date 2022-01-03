5161adbffcefe0a90e367e8760e3393523c42cb5
TheSSlimeyDevloper <theslimeydevyt@gmail.com>
2022-01-03 22:04:21 +0530

    init

--- Slimey.py
+++ Slimey.py
@@ -27,7 +27,7 @@ async def on_ready ():
   #f"{len(bot.guilds)} servers | <help"
   #status=discord.Status.idle
   #await bot.change_presence(status=discord.Status.online, activity=discord.ActivityType.watching, name=f"{len(bot.guilds)} servers | <help")
-  await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a movie"))
+  await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(bot.guilds)} servers | <help"))
   print(f"Logged in as {bot.user} (ID: {bot.user.id})")
   print("----------")
 
