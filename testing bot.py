035e7e123051e0c4056b2a4132328d768cbc2dae
TheSSlimeyDevloper <theslimeydevyt@gmail.com>
2022-01-07 19:25:23 +0530

    testing some new data base and json stuff

--- testing bot.py
+++ testing bot.py
@@ -1,10 +1,17 @@
 import discord
 from discord import emoji
+from discord import user
+from discord.commands import context
 from discord.ext import commands
 from discord.ext.commands.bot import Bot
 from discord.ext.commands.core import guild_only
+from discord.flags import alias_flag_value
 from discord.ui import Button, View
+import json
+import os
+import random
 
+os.chdir("C:\\Users\\Nesar.K\\Desktop\\Testing bot\\mainbank.json")
 
 bot = commands.Bot(command_prefix=".", help_command=None)
 
@@ -31,4 +38,63 @@ async def test(ctx):
     await ctx.send("Test button command!", view = view)
 
 
-bot.run("OTI0NTc3NDMxOTg0MTQ0Mzk0.Ycgl1Q.68y0DVTJrQ8zfIHOcE1IFdNCWbQ")
+bot.command(aliases = ["bal"])
+async def balance(ctx):
+    await open_account(ctx.author)
+
+    user = ctx.author
+    users = get_bank_data()
+
+    wallet_amt = users[str(user.id)]["wallet"]
+    bank_amt = users[str(user.id)]["bank"]
+
+    em = discord.Embed(title =  f"{ctx.author.name}'s balance", color = discord.Color.blue())
+    em.add_field(name = "Wallet balance", value = wallet_amt)
+    em.add_field(name = "Bank balance", value = bank_amt)
+
+    await ctx.send(embed = em)
+
+
+@bot.command()
+async def beg(ctx):
+    await open_account(ctx.author)
+
+    users = get_bank_data()
+
+    user = ctx.author
+
+    earnings = random.randrange(101)
+
+    await ctx.send(f"Someone gave you {earnings} coins!")
+
+    wallet_amt = users[str(user.id)]["wallet"] + earnings
+
+    with open("mainbainkk.json", "w") as f:
+        json.dump(users, f)
+
+
+async def open_account(user):
+
+    users = await get_bank_data()
+
+    if str(user.id) in users:
+        return False
+    else:
+
+        users[str(user.id)] = {}
+        users[str(user.id)]["wallet"] = 0
+        users[str(user.id)]["bank"] = 0
+
+    with open("mainbainkk.json", "w") as f:
+        json.dump(users, f)
+    return True
+
+
+async def get_bank_data():
+    with open("mainbainkk.json", "r") as f:
+        users = json.load(f)
+    
+    return users
+
+
+bot.run("OTI0NTc3NDMxOTg0MTQ0Mzk0.Ycgl1Q.2T-vJSp_xl1OUJxTmvdL1IwGlxk")
