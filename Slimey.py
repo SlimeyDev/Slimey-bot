b7df427cbe29367a011d1f0d8f0a953a917b7ddf
TheSlimeyDevloper <96341847+TheSSlimeyDevloper@users.noreply.github.com>
2022-01-24 12:26:44 +0530

    fixed `invite` command

--- Slimey.py
+++ Slimey.py
@@ -353,7 +353,7 @@ async def help(ctx, mode: typing.Optional[str]):
 
 @bot.command()
 async def invite(ctx):
-    await ctx.reply("click here to invite me!", url = "https://discord.com/api/oauth2/authorize?client_id=915488552568123403&permissions=8&scope=bot%20applications.commands")
+    await ctx.reply("https://discord.com/api/oauth2/authorize?client_id=915488552568123403&permissions=8&scope=bot%20applications.commands")
 
 
 @bot.command()
