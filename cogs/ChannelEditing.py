6a5b74ba0c526f40784dbf7f17b97e015dc99300
TheSlimeyDevloper <96341847+TheSSlimeyDevloper@users.noreply.github.com>
2022-02-14 15:29:25 +0530

    fixes

--- cogs/ChannelEditing.py
+++ cogs/ChannelEditing.py
@@ -12,7 +12,6 @@ class ChannelEditing(commands.Cog):
         channel = await guild.create_text_channel(channel_name)
 
     @commands.command(aliases = ["sm"])
-    @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.has_permissions(manage_messages=True)
     async def slowmode(ctx, seconds: int = 0):
 
@@ -29,6 +28,15 @@ class ChannelEditing(commands.Cog):
             else:
                 await ctx.channel.edit(slowmode_delay=seconds)
                 await ctx.send(f"Set the slowmode in this channel to {seconds} seconds!")
+    
+    @commands.Cog.listener()
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+
+            em = discord.Embed(title="<:Slimey_x:933232568055267359> Permission Error",
+                            description="You don't have the permission(s) to do that!", color=discord.Colour.red())
+
+        await ctx.reply(embed=em)
 
 def setup(bot):
     bot.add_cog(ChannelEditing(bot))
