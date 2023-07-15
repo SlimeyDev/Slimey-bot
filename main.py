b713bfa47a48186384bd3977173f3bb37691290f
SlimeyDev <96341847+SlimeyDev@users.noreply.github.com>
2023-07-15 19:21:04 +0530

    corrected help command display

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ async def help(ctx, mode: str = None):
       em = discord.Embed(
         title="👀 Utility/other commands:",
         description=
-        f"`<youtube`\n`<website`\n`<invite`\n`<report`\n`<info`\n`/avatar`\n`<countdown`\n`<discord`\n`<ping`\n`<snipe`",
+        f"`<youtube`\n`<website`\n`<invite`\n`<report`\n`<info`\n`<avatar`\n`<countdown`\n`<discord`\n`<ping`\n`<snipe`",
         color=discord.Color.purple())
 
       await ctx.reply(embed=em)
